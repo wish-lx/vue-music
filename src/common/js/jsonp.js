19b572dbc4104ee6423e88d35ca14de744fa2eec
zhaolingxue <18222478537@163.com>
2018-06-22 18:20:10 +0800

    '轮播图数据抓获'

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -4,9 +4,9 @@ export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
   return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
-      if(!err) {
+      if (!err) {
         resolve(data)
-      }else{
+      } else {
         reject(err)
       }
     })
@@ -15,8 +15,8 @@ export default function jsonp(url, data, option) {
 
 function param(data) {
   let url = ''
-  for(var k in data) {
-    let value = data[k] != undefined ? data[k] : ''
+  for (var k in data) {
+    let value = data[k] !== undefined ? data[k] : ''
     url += `&${k}=${encodeURIComponent(value)}`
   }
   return url ? url.substring(1) : ''
--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
 
+
 import 'common/stylus/index.styl'
+
+
 
 fastclick.attach(document.body)
 Vue.config.productionTip = false
+
+
 
 /* eslint-disable no-new */
 new Vue({
