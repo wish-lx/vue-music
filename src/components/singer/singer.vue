0c502b584e9670c8643bf45197c4112826c50f41
zhaolingxue <18222478537@163.com>
2018-06-28 15:20:43 +0800

    'singer数据规范化'

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -6,6 +6,10 @@
 <script type="text/ecmascript-6">
 import {getSingerList} from 'api/singer'
 import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
  export default {
     data() {
       return {
@@ -20,12 +24,45 @@ import {ERR_OK} from 'api/config'
          getSingerList().then((res) => {
            if (res.code === ERR_OK) {
              this.singers = res.data.list
-             console.log(this.singers)
+             console.log(_normalizeSinger(this.singers))
            }
          })
+      },
+      // 规范化singer数据
+      _normalizeSinger(list) {
+          let map = {
+            hot: {
+              title: HOT_NAME,
+              items: []
+            }
+          }
+           list.forEach((item, index) => {
+             if (index < HOT_SINGER_LEN) {
+               map.hot.items.push(
+                 new Singer({
+                   id: item.Fsinger_mid,
+                   name: item.Fsinger_name
+                 })
+               )
+             }
+              const key = item.Findex
+              if (!map[key]) {
+                map[key] = {
+                  title: key,
+                  items: []
+                }
+              }
+              map[key].items.push(
+                new Singer({
+                  id: item.Fsinger_mid,
+                  name: item.Fsinger_name
+                })
+              )
+           })
+           console.log(map)
+         }
       }
-    }
- }
+}
 </script>
 <style scoped lang="stylus" rel="stylesheet/stylus" >
   .singer 
