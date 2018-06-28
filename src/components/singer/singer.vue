cd22a06a7b326a508ba3585c25374f12b6efd75a
zhaolingxue <18222478537@163.com>
2018-06-28 15:39:50 +0800

    'singer数据排列顺序'

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -59,7 +59,21 @@ const HOT_SINGER_LEN = 10
                 })
               )
            })
-           console.log(map)
+          // 为了得到有序数列，我们需要处理map
+          let hot = []
+          let ret = []
+          for (let key in map) {
+            let val = map[key]
+            if (val.title.match(/[a-zA-Z]/)) {
+                ret.push(val)
+            } else if (val.title === HOT_NAME) {
+              hot.push(val)
+            }
+          }
+          ret.sort((a, b) => {
+            return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+          })
+          return hot.concat(ret)
          }
       }
 }
