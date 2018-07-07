329f511e68f1ee8fa5de360e8a1f0cb864dd8a15
zhaolingxue <18222478537@163.com>
2018-07-07 21:56:01 +0800

    'loading'

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <div class="icon-back"></div>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div class="play" v-show="songs.length>0" ref="playBtn">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -13,14 +19,16 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs" ></song-list>
       </div>
-      
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
-
+import loading from  'base/loading/loading'
 const RESERVED_HEIGHT = 40
  export default {
     props: {
@@ -54,6 +62,9 @@ const RESERVED_HEIGHT = 40
     methods: {
       scroll(pos) {
        this.scrollY = pos.y
+      },
+      back() {
+       this.$router.back()
       }
     },
     watch: {
@@ -76,9 +87,12 @@ const RESERVED_HEIGHT = 40
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
          this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
+          
         }
          this.$refs.bgImage.style.zIndex = zIndex
          this.$refs.bgImage.style['transform'] = `scale(${scale})`
@@ -92,7 +106,8 @@ const RESERVED_HEIGHT = 40
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      loading
     }
  }
 </script>
@@ -136,30 +151,30 @@ const RESERVED_HEIGHT = 40
         padding-top: 70%
         transform-origin: top
         background-size: cover
-        // .play-wrapper
-        //   position: absolute
-        //   bottom: 20px
-        //   z-index: 50
-        //   width: 100%
-        //   .play
-        //     box-sizing: border-box
-        //     width: 135px
-        //     padding: 7px 0
-        //     margin: 0 auto
-        //     text-align: center
-        //     border: 1px solid $color-theme
-        //     color: $color-theme
-        //     border-radius: 100px
-        //     font-size: 0
-        //     .icon-play
-        //       display: inline-block
-        //       vertical-align: middle
-        //       margin-right: 6px
-        //       font-size: $font-size-medium-x
-        //     .text
-        //       display: inline-block
-        //       vertical-align: middle
-        //       font-size: $font-size-small
+        .play-wrapper
+          position: absolute
+          bottom: 20px
+          z-index: 50
+          width: 100%
+          .play
+            box-sizing: border-box
+            width: 135px
+            padding: 7px 0
+            margin: 0 auto
+            text-align: center
+            border: 1px solid $color-theme
+            color: $color-theme
+            border-radius: 100px
+            font-size: 0
+            .icon-play
+              display: inline-block
+              vertical-align: middle
+              margin-right: 6px
+              font-size: $font-size-medium-x
+            .text
+              display: inline-block
+              vertical-align: middle
+              font-size: $font-size-small
       .filter
         position: absolute
         top: 0
