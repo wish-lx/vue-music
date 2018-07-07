0b199641b4169d9916b0fe90decadb7d9ea4ba2e
zhaolingxue <18222478537@163.com>
2018-07-07 14:49:41 +0800

    'song-list组件开发'

---
+++ src/base/song-list/song-list.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="song-list">
+   <ul>
+     <li v-for="song in songs" class="item">
+       <div class="content">
+         <h2 class="name">{{song.name}}</h2>
+         <p class="desc">{{getDesc(song)}}</p>
+       </div>
+     </li>
+   </ul>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+
+ export default {
+    props: {
+      songs: {
+        type: Array,
+        default: ''
+      }
+    },
+    methods: {
+      getDesc(song) {
+        return `${song.singer} - ${song.album}`
+      }
+    }
+ }
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus" >
+  
+
+</style>
