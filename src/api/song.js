2202331fc23213d8f82650e75bd236b17d4b27ec
zhaolingxue <18222478537@163.com>
2018-07-26 12:41:51 +0800

    ''

--- src/api/song.js
+++ src/api/song.js
@@ -5,11 +5,11 @@ export function getLyric(mid) {
   const url = '/api/lyric'
   const data = Object.assign({}, commonParams, {
     songmid: mid,
-    pcachetime: +new Date(),
     platform: 'yqq',
     hostUin: 0,
     needNewCode: 0,
-    g_tk: 67232076,
+    categoryId: 10000000,
+    pcachetime: +new Date(),
     format: 'json'
   })
   return axios.get(url, {
